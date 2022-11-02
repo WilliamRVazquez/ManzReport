da8cd3253aacde578a745132ba79195c255ba6cc
ernesto2001-code <ejimenez17@ucol.mx>
2022-11-01 19:29:43 -0600

    Solucion de bugs y mostrar imagen en el reporte

--- app/src/main/java/com/example/manzreport/crear_reportes.java
+++ app/src/main/java/com/example/manzreport/crear_reportes.java
@@ -4,15 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -22,10 +26,12 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,13 +40,14 @@ import java.util.UUID;
 public class crear_reportes extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     ImageButton atras;
     EditText ubicacion, DescReport;
-
+    ImageView imageView;
     StorageReference storageReference;
     String storage_path = "report/*";
     String download_uri;
 
     LinearLayout linearbtn;
-    Button btnedit, btndelete;
+    Button btndelete;
+    //Button btnedit;
 
     Spinner report;
 
@@ -49,7 +56,7 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
     String [] opciones = {"Tipo de incidencia","Capdam","Proteccion civil","Jardineria","Mantenimiento publico"};
 
     String item;
-
+    ProgressDialog progressDialog;
 
     Button btn_image, btn_enviar;
 
@@ -72,8 +79,10 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
         btn_enviar = (Button) findViewById(R.id.btn_enviar);
         btn_image = (Button) findViewById(R.id.btn_image);
         linearbtn = (LinearLayout) findViewById(R.id.layoutbtn);
-        btnedit = (Button) findViewById(R.id.btnEdit);
+        //btnedit = (Button) findViewById(R.id.btnEdit);
         btndelete = (Button) findViewById(R.id.btnRemove);
+        progressDialog = new ProgressDialog(this);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
 
         ubicacion = (EditText) findViewById(R.id.ubicacion_manual);
@@ -102,18 +111,8 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
         btn_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String idUser = mAuth.getCurrentUser().getUid();
-                DocumentReference id = mfirestore.collection("Reportes").document();
 
-                Map<String, Object> map = new HashMap<>();
-                map.put("id_user", idUser);
-                map.put("photo", "");
-                map.put("id", id.getId());
-                map.put("ubicacion", "");
-                map.put("Descripcion", "");
-                map.put("Tipo reporte", "");
-                mfirestore.collection("Reportes").document(id.getId()).set(map);
-                idd = id.getId();
+
                 uploadPhoto();
             }
         });
@@ -124,20 +123,19 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
                 Toast.makeText(crear_reportes.this, "Foto eliminada", Toast.LENGTH_SHORT).show();
                 linearbtn.setVisibility(View.GONE);
                 btn_image.setVisibility(View.VISIBLE);
+                imageView.setVisibility(View.GONE);
 
 
             }
         });
-        btnedit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                uploadPhoto();
-                editSi = "SI";
-                linearbtn.setVisibility(View.VISIBLE);
+        //btnedit.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View view) {
+                //uploadPhoto();
+              //  editSi = "SI";
 
-
-            }
-        });
+            //}
+        //});
 
 
         btn_enviar.setOnClickListener(new View.OnClickListener() {
@@ -162,7 +160,6 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
 
     }
 
-
     private void uploadPhoto() {
 
         Intent i = new Intent(Intent.ACTION_PICK);
@@ -170,7 +167,6 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
 
 
         startActivityForResult(i, COD_SEL_IMAGE);
-        linearbtn.setVisibility(View.VISIBLE);
 
     }
     @Override
@@ -181,18 +177,28 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
                 String ubireport = ubicacion.getText().toString().trim();
                 String desreporte = DescReport.getText().toString().trim();
                 subirPhoto(image_url ,ubireport, desreporte);
-                linearbtn.setVisibility(View.VISIBLE);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void subirPhoto(Uri image_url, String ubireport, String desreporte) {
+        progressDialog.setMessage("Añadiendo la imagen");
+        progressDialog.show();
+        String idUser = mAuth.getCurrentUser().getUid();
+        DocumentReference id = mfirestore.collection("Reportes").document();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("id_user", idUser);
+        map.put("photo", "");
+        map.put("id", id.getId());
+        map.put("ubicacion", "");
+        map.put("Descripcion", "");
+        map.put("Tipo reporte", "");
+        mfirestore.collection("Reportes").document(id.getId()).set(map);
+        idd = id.getId();
         String rute_storage_photo = storage_path + "" + photo + "" + mAuth.getUid() +""+ idd;
         StorageReference reference = storageReference.child(rute_storage_photo);
-
-
-
 
 
 
@@ -206,26 +212,34 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
                         @Override
                         public void onSuccess(Uri uri) {
                             download_uri = uri.toString();
-                            if (editSi.equals("SI")){
-                                Map<String, Object> map = new HashMap<>();
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("photo", download_uri);
+                            mfirestore.collection("Reportes").document(idd).update(map);
+                            imageView.setVisibility(View.VISIBLE);
+                            btn_image.setVisibility(View.GONE);
+                            linearbtn.setVisibility(View.VISIBLE);
+                            btn_enviar.setVisibility(View.VISIBLE);
+                            getimage(idd);
 
-                                map.put("photo", download_uri);
-
-                                mfirestore.collection("Reportes").document(idd).update(map);
-                                Toast.makeText(crear_reportes.this, "Se edito la imagen", Toast.LENGTH_SHORT).show();
-                                linearbtn.setVisibility(View.VISIBLE);
-                                btn_image.setVisibility(View.GONE);
-                            }else{
-                                Map<String, Object> map = new HashMap<>();
-                                map.put("photo", download_uri);
-                                mfirestore.collection("Reportes").document(idd).update(map);
-                                Toast.makeText(crear_reportes.this, "Se agrego la imagen", Toast.LENGTH_SHORT).show();
-                                btn_image.setVisibility(View.GONE);
-                                linearbtn.setVisibility(View.VISIBLE);
-
-                            }
+                            progressDialog.dismiss();
+                            Toast.makeText(crear_reportes.this, "Se agrego la imagen", Toast.LENGTH_SHORT).show();
 
 
+                            //if (editSi.equals("SI")){
+                               // Map<String, Object> map = new HashMap<>();
+                               // map.put("photo", download_uri);
+                                //mfirestore.collection("Reportes").document(idd).update(map);
+                                //Toast.makeText(crear_reportes.this, "Se edito la imagen", Toast.LENGTH_SHORT).show();
+                                //linearbtn.setVisibility(View.VISIBLE);
+                            //btn_image.setVisibility(View.GONE);
+                            //}else{
+                            // Map<String, Object> map = new HashMap<>();
+                              //  map.put("photo", download_uri);
+                                //mfirestore.collection("Reportes").document(idd).update(map);
+                               // Toast.makeText(crear_reportes.this, "Se agrego la imagen", Toast.LENGTH_SHORT).show();
+                               // btn_image.setVisibility(View.GONE);
+                                //linearbtn.setVisibility(View.VISIBLE);
+                            //}
                         }
                     });
                 }
@@ -234,6 +248,28 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
             @Override
             public void onFailure(@NonNull Exception e) {
                 Toast.makeText(crear_reportes.this, "Error al cargar foto", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getimage(String idd) {
+        mfirestore.collection("Reportes").document(idd).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String image = documentSnapshot.getString("photo");
+                try {
+                    if(!image.equals("")){
+
+                        Picasso.with(crear_reportes.this).load(image).resize(350, 350).into(imageView);
+                    }
+                }catch (Exception e){
+                    Log.v("Error", "e: " + e);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
             }
         });
     }
@@ -265,6 +301,7 @@ public class crear_reportes extends AppCompatActivity implements AdapterView.OnI
     protected void onStart() {
         super.onStart();
         linearbtn.setVisibility(View.GONE);
+        btn_enviar.setVisibility(View.GONE);
     }
 
     @Override
